4a21ecaa03a2e519f8c7b13ebd70a0a880a05167
Wilmer Rodriguez <wilmer.rodriguez-r@mail.escuelaing.edu.co>
2023-11-01 21:50:10 -0500

    Finiquitado

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,9 @@
-import { useState } from 'react'
-import { ChakraProvider, Box, AbsoluteCenter, Center, Text, Flex, Button, HStack } from '@chakra-ui/react'
+import { Header } from "./components/Header"
+import { ChakraProvider } from "@chakra-ui/react"
 function App() {
-  const containerStyles = {
-    background: "url('https://img.freepik.com/free-photo/glowing-lines-human-heart-3d-shape-dark-background-generative-ai_191095-1435.jpg?size=626&ext=jpg&ga=GA1.1.386372595.1698192000&semt=ais')",
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "center center",
-    minHeight: "100vh",
-  };
   return (
     <ChakraProvider>
-      <Box style={containerStyles}>
-        <Center minHeight="100vh">
-          <Box minHeight="100px" minW={"500px"} content=''>
-            <Center>
-              <Flex direction="column" align="center" justify="center">
-                <Text fontSize='6xl'  as='b'>Domina el Terreno</Text>
-                <Box position='relative' h='100px'>
-                  <AbsoluteCenter p='4' axis='both'>
-                    <HStack>
-                      <Button textColor="white" size={"lg"} variant='outline'>Ver Detalles</Button>
-                      <Button textColor="white" size={"lg"} variant='outline'>Ver Video</Button>
-                    </HStack>
-                  </AbsoluteCenter>
-                </Box>
-              </Flex>
-            </Center>
-          </Box>
-        </Center>
-      </Box>
+      <Header></Header>
     </ChakraProvider>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import { ChakraProvider, Box, AbsoluteCenter, Center, Text, Flex, Button, HStack } from '@chakra-ui/react'
+export const Header = () => {
+    const containerStyles = {
+        background: "url('https://img.freepik.com/free-photo/glowing-lines-human-heart-3d-shape-dark-background-generative-ai_191095-1435.jpg?size=626&ext=jpg&ga=GA1.1.386372595.1698192000&semt=ais')",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center center",
+        minHeight: "100vh",
+    };
+    return (
+        <Box style={containerStyles}>
+            <Center minHeight="100vh">
+                <Box minHeight="100px" minW={"500px"} content=''>
+                    <Center>
+                        <Flex direction="column" align="center" justify="center">
+                            <Text fontSize='6xl'  as='b'>Domina el Terreno</Text>
+                            <Box position='relative' h='100px'>
+                            <AbsoluteCenter p='4' axis='both'>
+                                <HStack>
+                                <Button textColor="white" size={"lg"} variant='outline'>Ver Detalles</Button>
+                                <Button textColor="white" size={"lg"} variant='outline'>Ver Video</Button>
+                                </HStack>
+                            </AbsoluteCenter>
+                            </Box>
+                        </Flex>
+                    </Center>
+                </Box>
+            </Center>
+        </Box>
+    );
+}
